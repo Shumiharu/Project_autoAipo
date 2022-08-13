28cefb3e75dd527f774da78b53405ac335f04f7b
Shumiharu <shimi2001haru0320@yahoo.co.jp>
2022-08-13 17:10:07 +0900

    autoAipo_docker ver.1.0.0.

--- autoAipo/autoAipo.js
+++ autoAipo/autoAipo.js
@@ -11,6 +11,9 @@ const { resolve } = require('path');
 const chrome = require('selenium-webdriver/chrome');
 const chromeOptions = new chrome.Options();
 chromeOptions.excludeSwitches("enable-logging");
+chromeOptions.setUserPreferences({
+  'download.default_directory': './downloads'
+});
 chromeOptions.addArguments('--headless');
 chromeOptions.addArguments('--no-sandbox');
 
@@ -24,22 +27,24 @@ let year;
 let month;
 // 今日の曜日
 let dayOfWeek = today.getDay();
-// (オプション)〇週間前 先週分は0を入れる
-let week = 0;
+// (オプション)○週間前(先週分は1を入れる)
+let week = 1;
+
 // 先週の日付を配列形式で返す（月曜日から金曜日）
-let calculateLastWeek = new Promise(function(resolve, reject) {
-  console.log("\n集計参照日:");
+let calculateLastWeek = new Promise(async function(resolve, reject) {
+  console.log("\n参照日は");
   let dates = [];
   let date = [];
   for(i=0; i<5; i++) {
     today = new Date();
-    dates[i] = new Date(today.setDate(today.getDate() - dayOfWeek - 6 - (week*7) + i));
+    dates[i] = new Date(today.setDate(today.getDate() - dayOfWeek - 6 - ((week-1)*7) + i));
     year = dates[i].getFullYear();
     month = ("00" + (dates[i].getMonth()+1)).slice(-2);
     date[i] = ("00" + dates[i].getDate()).slice(-2);
     dates[i] = year + "/" + month + "/" + date[i];
     console.log(dates[i]);
   }
+  console.log("です．\n");
   resolve(dates);
 })
 
@@ -55,12 +60,6 @@ let isSameMonth = calculateLastWeek.then(function(dates){
 let membersData = members.member;
 
 // ダウンロードフォルダ内のファイル検索
-// let downloads = fs.readdirSync('/Users/'+ os.userInfo().username +'/Downloads', function(error, files){
-//   if(error) {
-//     console.log(error);
-//   }
-// });
-
 let downloads = fs.readdirSync('./downloads', function(error, files){
   if(error) {
     console.log(error);
@@ -75,12 +74,11 @@ try {
  
   if(timecardsXls.length) {
     for(i=0; i<timecardsXls.length; i++) {
-      // fs.unlinkSync('/Users/' + os.userInfo().username + '/Downloads/' + timecardsXls[i]);
-      fs.unlinkSync('./downloads' + timecardsXls[i]);
+      fs.unlinkSync('./downloads/' + timecardsXls[i]);
     }
-    console.log("\ntimecard.xls were deleted completely.");
+    // console.log("\ntimecard.xls were deleted completely.\n");
   } else {
-    console.log("No timecard.xls in this directory...");
+    // console.log("No timecard.xls in this directory...\n");
   }
 } catch(error) {
   throw error;
@@ -93,12 +91,12 @@ driver.get('http://sv1.comm.nitech.ac.jp')
   await driver.wait(until.titleIs("Aipo"), 10000);
 }).then(async function(){
   // もとから設定しておくタイプ（定時実行したいときはこちらから）
-  let userName = "haruki";
-  let userPassword = "8280";
+  // let userName = "";
+  // let userPassword = "";
 
   // ユーザー名とパスワードを入力させるタイプ
-  // let userName = await readUserInput("ユーザー名: ");
-  // let userPassword = await readUserInput("パスワード: ");
+  let userName = await readUserInput("ユーザー名を入力してください: ");
+  let userPassword = await readUserInput("パスワードを入力してください: ");
 
   let inputName = await driver.findElement({ id: 'member_username' });
   let inputPassword = await driver.findElement({ id: 'password'});
@@ -116,39 +114,100 @@ driver.get('http://sv1.comm.nitech.ac.jp')
   return driver.wait(until.titleContains("Aipo"), 10000);
 }).then(function(){
   // Aipo8からtimecard.xlsファイルをダウンロードし，ダウンロードフォルダからそれらを取得する
-  console.log("Welcome to Aipo8! Now downloading...");
+  console.log("\nWelcome to Aipo8! Now downloading...");
   return new Promise(async function(resolve, reject){
     await isSameMonth.then(async function(result){
-      let timer;
+      let count = 0;
+      let downloadInterval = 300;
+      let detectInterval = downloadInterval/10;
+      let number, download, detection;
+      // 月をまたがない場合
       if(result) {
-        timer = 4000;
-        for(i=0; i<membersData.length; i++) {
-          await driver.get('http://sv1.comm.nitech.ac.jp/portal/template/ExtTimecardXlsExportScreen/target_user_id/'+ membersData[i].id + '/view_month/' + year + '-' + month +'/f/timecard.xls');
-        }
+        number = 0;
+         // 一定時間ごとに
+        download = setInterval(function() {
+          if(count < membersData.length && number < membersData.length) {
+            // ダウンロードを試みる
+            driver.get('http://sv1.comm.nitech.ac.jp/portal/template/ExtTimecardXlsExportScreen/target_user_id/'+ membersData[number].id + '/view_month/' + year + '-' + month +'/f/timecard.xls')
+            .then(function(){
+              // 一定時間ごとに
+              detection = setInterval(function(){
+                // downloadsフォルダにtimecard.xlsファイルがないかチェックする
+                if(fs.existsSync('./downloads/timecard.xls')){
+                  // headless（CUI上で実行する場合）はダウンロードしたものがなぜか上書きされるので逐次別ファイルに置き換える
+                  fs.renameSync('./downloads/timecard.xls', './downloads/timecard(' + count + ').xls', function(error){
+                    if(error) {
+                      throw error;
+                    }
+                  });
+                  // 置き換えたファイルを取得
+                  let renamed = fs.statSync('./downloads/timecard(' + count + ').xls');
+                  // ファイルサイズが0（処理が速く，二重でtimecard.xlsが検出されると空ファイルを生成してしまう）ではないとき
+                  if(renamed.size != 0) {
+                    // 次のファイルを生成するために
+                    count++;
+                    // setIntervalの終了
+                    clearInterval(detection);
+                  }
+                }    
+              }, detectInterval);
+            })
+            .catch(function(error){
+              throw error;
+            })
+          } else {
+            // setIntervalの終了
+            clearInterval(download);
+          }
+          number++;
+        }, downloadInterval)
+        return (downloadInterval+detectInterval)*(membersData.length);
+      
+      // 月をまたぐ場合
       } else {
-        timer = 6000;
-        for(i=0; i<membersData.length; i++) {
-          await driver.get('http://sv1.comm.nitech.ac.jp/portal/template/ExtTimecardXlsExportScreen/target_user_id/'+ membersData[i].id + '/view_month/' + year + '-' + (month - 1) +'/f/timecard.xls');
-          await driver.get('http://sv1.comm.nitech.ac.jp/portal/template/ExtTimecardXlsExportScreen/target_user_id/'+ membersData[i].id + '/view_month/' + year + '-' + month +'/f/timecard.xls');
-        }
+        // ほとんど同じ
+        number = 0;
+        download = setInterval(function() {
+          if(count < membersData.length*2 && number < membersData.length) {
+            // 2ヵ月分ダウンロードするように
+            for(i=0; i<2; i++) {
+              driver.get('http://sv1.comm.nitech.ac.jp/portal/template/ExtTimecardXlsExportScreen/target_user_id/'+ membersData[number].id + '/view_month/' + year + '-' + (month - i) +'/f/timecard.xls')
+              .then(function(){
+                detection = setInterval(function(){
+                  if(fs.existsSync('./downloads/timecard.xls')){
+                    fs.renameSync('./downloads/timecard.xls', './downloads/timecard(' + count + ').xls', function(error){
+                      if(error) {
+                        throw error;
+                      }
+                    });
+                    let renamed = fs.statSync('./downloads/timecard(' + count + ').xls');
+                    if(renamed.size != 0) {
+                      count++;
+                      clearInterval(detection);
+                    }
+                  }    
+                }, detectInterval);
+              })
+              .catch(function(error){
+                throw error;
+              })
+            }
+          } else {
+            clearInterval(download);
+          }
+          number++;
+        }, downloadInterval)
+        return (downloadInterval+detectInterval)*(membersData.length*2);
       }
-      return(timer);
     }).then(function(timer){
-      console.log("Please wait...");
       // ファイルのダウンロード時間を考慮
       setTimeout(async function(){
-        // let newDownloads = fs.readdirSync('/Users/' + os.userInfo().username + '/Downloads', function(error, files){
-        //   if(error) {
-        //     console.log(error);
-        //   }
-        //   return files;
-        // });
         let newDownloads = fs.readdirSync('./downloads', function(error, files){
-            if(error) {
-              console.log(error);
-            }
-            return files;
-          });
+          if(error) {
+            console.log(error);
+          }
+          return files;
+        });
         resolve(newDownloads.filter(function(files){
           return /timecard/.test(files);
         }));
@@ -166,16 +225,14 @@ driver.get('http://sv1.comm.nitech.ac.jp')
     if(files.length) {
       console.log("timecards.xls (" + files.length +" files) were downloaded successfully.");
       for(i=0; i<files.length; i++) {
-        // xlsxFiles[i] = XLSX.readFile('/Users/' + os.userInfo().username + '/Downloads/' + files[i], {cellDates: true});
         xlsxFiles[i] = XLSX.readFile('./downloads/' + files[i], {cellDates: true});
         sheetName = xlsxFiles[i].SheetNames;
         sheet = xlsxFiles[i].Sheets[sheetName[0]];
         json[i] = XLSX.utils.sheet_to_json(sheet); 
       }
-     
       resolve(json);
     } else {
-      console.log("Error: No timecards.xls file could be downloaded.");
+      console.log("Error: No timecards.xls file could be downloaded. Maybe, invalid username or password.");
       reject();
     }
   })
@@ -249,7 +306,7 @@ driver.get('http://sv1.comm.nitech.ac.jp')
       // 月曜日から金曜日まで
       let attendance = 0;
       var newJsonData = jsonData.filter(function(json){
-        if(json.曜日 == "火") {
+        if(json.曜日 == "Tue") {
           // 15:00以降に退勤していたら出席扱い
           let clockOut = json.退勤時間.split(':')[0];
           if(clockOut && clockOut >= 15) {
@@ -289,7 +346,7 @@ driver.get('http://sv1.comm.nitech.ac.jp')
         for(json of jsonData) {
           daysOfWeek.push(json.曜日);
         }
-        outMembers.push({氏名: jsonData[0].氏名, 日数: jsonData.length, 曜日: daysOfWeek.toString() + "曜日"});
+        outMembers.push({氏名: jsonData[0].氏名, 日数: jsonData.length, 曜日: daysOfWeek.toString() + "day"});
       }
     })
     resolve(outMembers);
@@ -301,13 +358,13 @@ driver.get('http://sv1.comm.nitech.ac.jp')
     for(var outMember of outMembers) {
       console.log(outMember.氏名 + "さん: " + outMember.日数 + "日 " + outMember.曜日);
     }
-    console.log("\n報告に関しては院生ゼミの出席やAipo上のスケジュールを確認してからお願いします\n");
+    // console.log("\n報告に関しては院生ゼミの出席やAipo上のスケジュールを確認してからお願いします\n");
   } else {
     console.log("\n先週のoutはいませんでした! おめでとうございます!!\n");
   }
 
   // 以下，メールテンプレ
-  console.log("\nお世話になっております．B4の清水です．先週の出席状況についてご報告させていただきます．\n\n先週のout:\n\nとなります．引き続きどうぞよろしくお願い致します．\n");
+  console.log("\n（メールテンプレート）\n\n岡本先生\n\nお世話になっております．先週の出席状況についてご報告させていただきます．\n\n先週のout:\n\nとなります．引き続きどうぞよろしくお願い致します．\n");
 
   driver.close();
 }).catch(function(error){
